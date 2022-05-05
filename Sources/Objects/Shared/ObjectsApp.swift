6d1d9ad0e86543d42393cfc492e89dbb51bfdde9
Kyungho Kim <kyungho.kim@yahoo.com>
2022-05-05 22:16:57 +0900

    Xcode 프로젝트 생성

---
+++ Sources/Objects/Shared/ObjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectsApp.swift
+//  Shared
+//
+//  Created by 김경호 on 2022/05/05.
+//
+
+import SwiftUI
+
+@main
+struct ObjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
